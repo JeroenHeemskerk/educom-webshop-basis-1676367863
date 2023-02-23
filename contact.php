a2f79d9c9635bd287a3a891ee4ede216e9a901d6
Ruben van der Zouw <rubenvdzouw@gmail.com>
2023-02-23 10:32:42 +0100

    Issue 1.3B #5

--- contact.php
+++ contact.php
@@ -7,6 +7,7 @@
         
     </head>
         <body>
+            
                 <ul id="menu">
                     <li id="keuzemenu"><a href="index.html" class="inactive">Home</a></li>
                     <li id="keuzemenu"><a href="about.html" class="inactive">About</a></li>
@@ -14,52 +15,61 @@
                 </ul>
 <div class="content">
       <h2>Contact opnemen?</h2>
-<?php          
- 
- function validateContactform()
- {
+<?php  
 
+
+ 
 $nameErr = $emailErr = $telefoonErr = $favcontactErr = $commentErr = "";
 $aanhef = $name = $email = $telefoon = $favcontact = $comment = "";
 $valid = false;
 
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
-        $nameErr = " * Vul uw naam in";
-        } else { $name = test_input($_POST["name"]);
+        $nameErr="Vul uw naam in";
+    } else { 
+        $name=test_input($_POST["name"]);
     }
 
     if (empty($_POST["email"])) {
-        $emailErr = " * Vul een mailadres in";
-    } else { $email = test_input($_POST["email"]);
+        $emailErr ="Vul een mailadres in";
+    } else { 
+        $email=test_input($_POST["email"]);
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Vul een geldig emailadres in";
+            $emailErr="Vul een geldig emailadres in";
         }
     }
     if (empty($_POST["telefoon"])) {
-        $telefoonErr = " * Vul uw telefoonnummer";
+        $telefoonErr="Vul uw telefoonnummer";
     }
-    else  { $telefoon = test_input($_POST["telefoon"]); } 
+    else  { 
+        $telefoon=test_input($_POST["telefoon"]); 
+    } 
     
     if (empty($_POST["favcontact"])) {
-        $favcontactErr = " * Selecteer een contact optie";
+        $favcontactErr="Selecteer een contact optie";
     }   
-    else { $favcontact = test_input($_POST["favcontact"]); }
+    else { 
+        $favcontact=test_input($_POST["favcontact"]); 
+    }
     
     if (empty($_POST["comment"])) {
-        $commentErr = " * Vul uw reden voor contact in";
+        $commentErr="Vul uw reden voor contact in";
     }
-    else { $comment = test_input($_POST["comment"]); }
+    else { 
+        $comment=test_input($_POST["comment"]); 
     }
+    
 
-    if ( $nameErr === "" && $emailErr === "" && $telefoonErr === "" && $favcontactErr === "" && $commentErr === "" && $aanhef !== "" && $name !== "" && $email  !== "" && $telefoon !== "" && $favcontact !== "" && $comment !== "") {
+    if ( $nameErr === "" && $emailErr === "" && $telefoonErr === "" && $favcontactErr === "" &&  $commentErr === "" && 
+         $aanhef !== "" &&  $name !== "" && $email  !== "" && $telefoon !== "" && $favcontact !== "" && $comment !== "") {
         $valid = true;} 
-
+        
+    }
 
 ?>
-    <?php if ($valid) {  ?>
+    <section>
+    <?php if (!$valid) { ?>
                 <form  method="post" action="contact.php">; <!-- idee wat ik had was alles onder 1 form te doen zodat het als 1 gegeven verstuurd kan worden. -->
                                                          <!-- In het voorbeeld op W3schools hebben ze het over /action.php/ maar aangezien ik nog geen soort gelijk document hebt stuur ik ze terug naar de homepage.-->
                                                          <!-- bij deze instantie leek het mij beter om de method "post" te gebruiken omdat de textarea gevoelige gegevens kan bevatten. -->
@@ -70,19 +80,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </select>
                 <br>
                     <label class="NAW" for="name">Naam:</label><!-- kopje waar men hun naam kan invullen -->
-                        <input type="text" name="name" value="<?php echo $name; ?>" id="naam">
-                        <span class="error"><?php echo $nameErr ?></span><br>
+                        <input type="text" name="name" id="name" value="<?php echo $name;?>">
+                        <span class="error">* <?php echo $nameErr; ?></span><br>
                     <label class="NAW"for="email">E-mail:</label> <!-- kopje waar men hun email kan invullen -->
-                        <input type="text" name="email" value="<?php echo $email; ?>" id="mailadres">
-                        <span class="error"><?php echo $emailErr ?></span><br>
+                        <input type="text" name="email" id="mailadres" value="<?php echo $email;?>">
+                        <span class="error">* <?php echo $emailErr; ?></span><br>
                     <label class="NAW" for="telephone">Telefoonnummer:</label> <!-- kopje waar men hun Telefoonnummer kan invullen -->
-                        <input type="text" name="telefoon" value="<?php echo $telefoon; ?>" id="telefoonnummer">
-                        <span class="error"><?php echo $telefoonErr ?></span><br>   
+                        <input type="text" name="telefoon" id="telefoonnummer" value="<?php echo $telefoon;?>">
+                        <span class="error">* <?php echo $telefoonErr; ?></span><br>   
                     
                 <br>
                 <br>
                     <label for="contact">Hoe wilt u gecontacteerd worden:</label> <!-- In de opdracht stond communicatievoorkeur, maar ik vond dit wat netter staan -->
-                    <span class="error"><?php echo $favcontactErr ?></span>
+                    <span class="error">* <?php echo $favcontactErr; ?></span>
                     <br>
                 <br>
                     <input type="radio" id="favcontactphone" name="favcontact" value="per Telefoon"> <!-- de radio buttons met opties telefoon nummer & email -->
@@ -94,24 +104,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <label for="comment">Beschrijf in het kort waar u contact over wilt opnemen:</label> <!-- textarea waar men kort en bondig kan opschrijven waarover ze contact willen hebben -->
                 <br>
                 <br>
-                        <textarea name="comment" rows="10" cols="50" maxlength="250" value="<?php echo $comment; ?>"></textarea><br> <!-- ik heb gelijkt een maximum aantal characters toegevoegd zodat er geen gigantische verhalen verstuurd worden -->
-                        <span class="error"><?php echo $commentErr ?></span>
+                        <textarea name="comment" rows="10" cols="50" maxlength="250" value="<?php echo $comment;?>"></textarea><br> <!-- ik heb gelijkt een maximum aantal characters toegevoegd zodat er geen gigantische verhalen verstuurd worden -->
+                        <span class="error">* <?php echo $commentErr; ?></span>
                         
                 <br>
                         <input type="submit" name="versturen" value="Versturen">
                 </form>
-    <?php } else { ?>
-                    <p2>Bedankt voor uw bericht!</p2>
-                <?php } ?>
-                <?php  
-                            
-    }
-function test_input($data) {
-  $data = trim($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
+    
+                <?php } else { ?> 
+                   <?php echo "Bedankt" ?>
+                <?php } 
+        
+        function test_input($data) {
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }               
+      
+    ?>
+</section>
+
             </div>
+            <footer>&copy Created by Ruben van der Zouw 2023</footer>
         </body>
 </html>
